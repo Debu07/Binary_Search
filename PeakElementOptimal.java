cc70d42830d686d046f7281c285855361a6a4c43
vedant <mehtavedant7498@gmail.com>
2021-02-20 23:34:41 +0530

    Peak Element in an array (Optimal)

--- PeakElementOptimal.java
+++ PeakElementOptimal.java
@@ -8,7 +8,9 @@ public class PeakElementOptimal {
         int low=0,high= array.length-1;
         while(low<=high){
             int mid=(low+high)/2;
-            if (mid==0 || (array[mid]>array[mid+1]&& array[mid]>array[mid-1])) return array[mid];
+            if (mid==0 || (array[mid]>array[mid+1])&&
+                    (mid== array.length-2|| array[mid]>array[mid-1]))
+                return array[mid];
             if (mid>0 && array[mid-1]>array[mid]) high=mid-1;
             else low=mid+1;
         }
